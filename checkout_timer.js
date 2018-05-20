af61244543323892b33f70802a969564ec2fd4ee
Brian Bugle <bugleb@gmail.com>
2018-05-20 10:46:17 -0400

    added disclaimer

--- checkout_timer.js
+++ checkout_timer.js
@@ -32,7 +32,7 @@ function startTimer(duration, display) {
 }
 window.onload = function () {
 	
-	var fragment3 = create('<div style="text-align:center; background:#EBE9E0;padding:10px 20px;border:1px solid #F4743C; font-size:20px; color:#2c2c2c; font-weight:bold;-webkit-border-radius: 5px; -moz-border-radius: 5px; border-radius: 5px; margin:10px 0px 20px 0px">Use coupon <span style="color:#F4743C;">LASTMINUTE</span> in the next <span id="time"></span> minutes to receive 15% off your order!</div>');
+	var fragment3 = create('<div style="text-align:center;background:#EBE9E0;padding:10px 20px;border:1px solid #F4743C;font-size:20px;color:#2c2c2c;font-weight: bold;-moz-border-radius: 5px;border-radius: 5px;margin:10px 0px 20px 0px;">Use coupon <span style="color:#F4743C;">LASTMINUTE</span> in the next <span id="time">09:18</span> minutes to receive 15% off your order!*<br><br><span style="font-size:12px;">* Offer not valid on discounted items (excluding sale items).<span></span></span></div>');
 	document.getElementsByClassName('main__header')[0].appendChild(fragment3);
 
 	var ten = 60 * 10,
