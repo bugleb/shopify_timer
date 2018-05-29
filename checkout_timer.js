0243f9a5c7abc62f0e4ee727c462c149c796b544
Brian Bugle <bugleb@gmail.com>
2018-05-29 15:25:25 -0400

    Added shipping disclaimer

--- checkout_timer.js
+++ checkout_timer.js
@@ -31,11 +31,8 @@ function startTimer(duration, display) {
     setInterval(timer, 1000);
 }
 window.onload = function () {
-	
-	var fragment3 = create('<div style="text-align:center;background:#EBE9E0;padding:10px 20px;border:1px solid #F4743C;font-size:20px;color:#2c2c2c;font-weight: bold;-moz-border-radius: 5px;border-radius: 5px;margin:10px 0px 20px 0px;">Use coupon <span style="color:#F4743C;">LASTMINUTE</span> in the next <span id="time">09:18</span> minutes to receive 15% off your order!*<br><br><span style="font-size:12px;">* Offer not valid on discounted items (excluding sale items).<span></span></span></div>');
-	document.getElementsByClassName('main__header')[0].appendChild(fragment3);
-
-	var ten = 60 * 10,
-	display = document.querySelector('#time');
-	startTimer(ten, display);
+    var discount = create('<div style="text-align:center;background:#EBE9E0;padding:10px 20px;border:1px solid #F4743C;font-size:20px;color:#2c2c2c;font-weight: bold;-moz-border-radius: 5px;border-radius: 5px;margin:10px 0px 20px 0px;">Use coupon <span style="color:#F4743C;">LASTMINUTE</span> in the next <span id="time">09:18</span> minutes to receive 15% off your order!*<br><br><span style="font-size:12px;">* Offer not valid on discounted items (excluding sale items).<span></span></span></div>');
+    document.getElementsByClassName('main__header')[0].appendChild(discount);
+    var shipping = create('<div style="text-align:center;">Note: ﻿Once you place your order, please allow 3-5 business days to process your orders. After that, it will take 12 - 20 business days for delivery in the United States, and 15-30 business days for international orders (depending on location).</div>');
+    document.getElementsByClassName('total-line-table__tbody')[0].appendChild(shipping);
 };
