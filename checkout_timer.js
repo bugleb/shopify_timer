e15b86cbd4a89f9937deed197f105bd3564d1ae9
Brian Bugle <bugleb@gmail.com>
2018-05-29 15:42:09 -0400

    Reduce font size

--- checkout_timer.js
+++ checkout_timer.js
@@ -33,6 +33,6 @@ function startTimer(duration, display) {
 window.onload = function () {
     var discount = create('<div style="text-align:center;background:#EBE9E0;padding:10px 20px;border:1px solid #F4743C;font-size:20px;color:#2c2c2c;font-weight: bold;-moz-border-radius: 5px;border-radius: 5px;margin:10px 0px 20px 0px;">Use coupon <span style="color:#F4743C;">LASTMINUTE</span> in the next <span id="time">09:18</span> minutes to receive 15% off your order!*<br><br><span style="font-size:12px;">* Offer not valid on discounted items (excluding sale items).<span></span></span></div>');
     document.getElementsByClassName('main__header')[0].appendChild(discount);
-    var shipping = create('<div style="text-align:center;">Note: ﻿Once you place your order, please allow 3-5 business days to process your orders. After that, it will take 12 - 20 business days for delivery in the United States, and 15-30 business days for international orders (depending on location).</div>');
+    var shipping = create('<div style="font-size:0.75em;">Note: ﻿Once you place your order, please allow 3-5 business days to process your orders. After that, it will take 12 - 20 business days for delivery in the United States, and 15-30 business days for international orders (depending on location).</div>');
     document.getElementsByClassName('order-summary__sections')[0].appendChild(shipping);
 };
