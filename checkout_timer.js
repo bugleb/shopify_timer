21d192ba63e6fe253782cf6f5add9d15522ef98a
Brian Bugle <bugleb@gmail.com>
2018-06-14 03:39:19 -0400

    Adds trust badges

--- checkout_timer.js
+++ checkout_timer.js
@@ -31,12 +31,15 @@ function startTimer(duration, display) {
     setInterval(timer, 1000);
 }
 window.onload = function () {
-    var discount = create('<div style="text-align:center;background:#EBE9E0;padding:10px 20px;border:1px solid #F4743C;font-size:20px;color:#2c2c2c;font-weight: bold;-moz-border-radius: 5px;border-radius: 5px;margin:10px 0px 20px 0px;">Use coupon <span style="color:#F4743C;">LASTMINUTE</span> in the next <span id="time">09:18</span> minutes to receive 10% off your order!*<br><br><span style="font-size:12px;">* Offer not valid on discounted items (excluding sale items).<span></span></span></div>');
-    document.getElementsByClassName('main__header')[0].appendChild(discount);
-    var shipping = create('<div style="font-size:0.75em;">Note: ﻿Once you place your order, please allow 3-5 business days to process your orders. After that, it will take 12 - 20 business days for delivery in the United States, and 15-40 business days for international orders (depending on location).</div>');
+    // var discount = create('<div style="text-align:center;background:#EBE9E0;padding:10px 20px;border:1px solid #F4743C;font-size:20px;color:#2c2c2c;font-weight: bold;-moz-border-radius: 5px;border-radius: 5px;margin:10px 0px 20px 0px;">Use coupon <span style="color:#F4743C;">LASTMINUTE</span> in the next <span id="time">09:18</span> minutes to receive 10% off your order!*<br><br><span style="font-size:12px;">* Offer not valid on discounted items (excluding sale items).<span></span></span></div>');
+    // document.getElementsByClassName('main__header')[0].appendChild(discount);
+    // var shipping = create('<div style="font-size:0.75em;">Note: ﻿Once you place your order, please allow 3-5 business days to process your orders. After that, it will take 12 - 20 business days for delivery in the United States, and 15-40 business days for international orders (depending on location).</div>');
     // document.getElementsByClassName('order-summary__sections')[0].appendChild(shipping);
 
-    var ten = 60 * 10,
-    display = document.querySelector('#time');
-    startTimer(ten, display);
+    // var ten = 60 * 10,
+    // display = document.querySelector('#time');
+    // startTimer(ten, display);
+
+    var badges = create('<div><img src="https://raw.githubusercontent.com/bugleb/shopify_timer/20561e0c37900efe3b440b9eb6d36250210b2e66/safe_checkout.png" /></div>');
+    document.getElementsByClassName('alt-payment-list-container')[0].appendChild(badges);
 };
